bc5b25724a900891fd2c87e7f0932404c8fa46aa
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-06-16 17:28:57 +0530

    Create our services & testimonial section

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,4 +178,51 @@
     duration: 800
   });
 
+  /* 5. Testimonial Active*/
+    var testimonial = $('.h1-testimonial-active');
+      if(testimonial.length){
+      testimonial.slick({
+          dots: false,
+          infinite: true,
+          speed: 1000,
+          autoplay:false,
+          arrows: true,
+          prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
+          nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+                arrow:true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrow:false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: false,
+                arrow:false
+              }
+            }
+          ]
+        });
+      }
+
+
 })(jQuery);
