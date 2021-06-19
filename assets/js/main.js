74a77bff5f8630bdfae78c7bfb4c2bcea88e197e
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-06-19 12:56:17 +0530

    Update timeline

--- assets/js/main.js
+++ assets/js/main.js
@@ -224,5 +224,33 @@
         });
       }
 
+      /**
+         * Testimonials slider
+         */
+        new Swiper('.testimonials-slider', {
+          speed: 600,
+          loop: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+          slidesPerView: 'auto',
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+          },
+          breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 20
+            }
+          }
+        });
 
 })(jQuery);
