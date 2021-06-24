15548e19b9051003448017b13d8e009bf083ea51
Abitharani Jeyachandran <abitharani009@gmail.com>
2021-06-25 00:53:31 +0530

    Update services & add partners

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,4 +253,40 @@
           }
         });
 
+        /**
+         * Clients Slider
+         */
+        new Swiper('.partner-slider', {
+          speed: 400,
+          loop: true,
+          autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+          },
+          slidesPerView: 'auto',
+          pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+          },
+          breakpoints: {
+            320: {
+              slidesPerView: 1,
+              spaceBetween: 20
+            },
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 20
+            },
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20
+            },
+            992: {
+              slidesPerView: 4,
+              spaceBetween: 20
+            }
+          }
+        });
+
 })(jQuery);
