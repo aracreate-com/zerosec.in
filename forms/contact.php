393b1152a9d4381771dcdd1488788297b7d915a0
vinothraj <Kanapathipillaivinothraj@gmail.com>
2022-11-23 10:42:05 +0530

    fix: change some code in contact page

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'Kanapathipillaivinothraj@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -38,4 +38,3 @@
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
-?>
